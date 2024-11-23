91ecf9b6902a0076f3d9aabe62ecc724c9d8613f
Everton Renan <everton.renan@clevertech.biz>
2024-11-23 13:41:40 -0300

    fix: typo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name='index '
+        name='index'
         options={{
           title: t('Liturgy'),
           tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
